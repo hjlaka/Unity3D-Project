6e8c7245488b708c18e8a3e6159871c3f20e827e
HeejinLee <windamongtree@gmail.com>
2022-12-30 20:06:48 +0900

    feat: 카메라 줌 추가

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
 
     [SerializeField]
     private float moveSpeed;
+    [SerializeField]
+    private float zoomSpeed;
 
     [SerializeField]
     private float padding;
@@ -14,6 +16,7 @@ public class CameraController : MonoBehaviour
     private void Update()
     {
         Move();   
+        Zoom();
     }
 
     private void Move()
@@ -25,9 +28,15 @@ public class CameraController : MonoBehaviour
         else if (Screen.width - padding <= pos.x && pos.x <= Screen.width)
             transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.World);
         else if (0 <= pos.y && pos.y <= padding)
-            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.back * moveSpeed * Time.deltaTime, Space.World);
         else if (Screen.height - padding <= pos.y && pos.y <= Screen.height)
-            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime, Space.World);
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.World);
     }
 
+    private void Zoom()
+    {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        transform.Translate(transform.forward * scroll * zoomSpeed * Time.deltaTime);       // 원하는대로 동작하지 않음
+    }
 }
