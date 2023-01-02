a45f84d27085376dd0c0f8f6a2bf2dfd5a37023d
HeejinLee <windamongtree@gmail.com>
2023-01-02 20:40:25 +0900

    feat: 선택한 기물 변경 기능 추가

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,45 +4,43 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
+
     public Place place;
+
+    public TeamData team;
 
     private Renderer render;
 
     [SerializeField]
     private Color mouseOver;
-    [SerializeField]
+    //[SerializeField]
     private Color normal;
 
-    
+    private List<Piece> defendFor;
+    private List<Piece> attackTo;
     private void Awake()
     {
         render = GetComponentInChildren<Renderer>();
+        defendFor = new List<Piece>();
+        attackTo = new List<Piece>();
     }
 
     private void Start()
     {
+        normal = team.normal;
         transform.position = place.transform.position;
         render.material.color = normal;
     }
 
-    private void OnMouseOver()
+    public void AddAttack(Piece piece)
     {
-        //Debug.Log(string.Format("{0} 마우스 가리킴", gameObject.name));
-        render.material.color = mouseOver;
+        attackTo.Add(piece);
+    }
+    public void AddDefence(Piece piece)
+    {
+        defendFor.Add(piece);
     }
 
-    private void OnMouseExit()
-    {
-        //Debug.Log(string.Format("{0} 밖으로 마우스 나감", gameObject.name));
-        render.material.color = normal;
-    }
-
-    private void OnMouseUpAsButton()
-    {
-        //Debug.Log(string.Format("{0} 클릭", gameObject.name));
-        PlaceManager.Instance.selectedPiece = this;
-        PlaceManager.Instance.ShowPlaceable();
-    }
 
     public void SetInPlace(Place place)
     {
@@ -62,5 +60,40 @@ public class Piece : MonoBehaviour
         return false;
     }
 
+
+    public virtual void LookAttackRange() { }
+
     public virtual void ShowMovable() { }
+
+    private void OnMouseOver()
+    {
+        //Debug.Log(string.Format("{0} 마우스 가리킴", gameObject.name));
+        render.material.color = mouseOver;
+    }
+
+    private void OnMouseExit()
+    {
+        //Debug.Log(string.Format("{0} 밖으로 마우스 나감", gameObject.name));
+        render.material.color = normal;
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        if (GameManager.Instance.state == GameManager.GameState.SELECTING_PIECE)
+        {
+            Debug.Log(string.Format("{0} 클릭", gameObject.name));
+            PlaceManager.Instance.SelectPiece(this);
+        }
+        
+        else if(GameManager.Instance.state == GameManager.GameState.SELECTING_PLACE)
+        {
+            // 같은 팀 기물이라면
+            PlaceManager.Instance.SelectedPieceInit();
+            PlaceManager.Instance.SelectPiece(this);
+
+            // 다른 팀 기물이라면
+            // 공격할 수 있다면
+        }
+    }
 }
+
--- Assets/Scripts/PlaceManager.cs
+++ Assets/Scripts/PlaceManager.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlaceManager : SingleTon<PlaceManager>
 {
-    public Piece selectedPiece;
+    [SerializeField]
+    private Piece selectedPiece;
+    public Piece SelectedPiece
+    {
+        get { return selectedPiece; }
+        set 
+        { 
+            selectedPiece = value;
+        }
+    }
+
+    public UnityEvent OnExitSelect;
 
     public Place selectedPlace;
 
 
     [SerializeField]
     private Color highlight;
+    [SerializeField]
+    private Color attackable;
 
 
     public void ShowPlaceable()
     {
-
+        
 
         Place curPlace = selectedPiece.place;
         Vector2Int curIndex = curPlace.boardIndex;
@@ -25,31 +39,50 @@ public class PlaceManager : SingleTon<PlaceManager>
         if (null == curBoard)
             return;
 
+        // 규칙을 따르지 않는 보드라면 종료
+        if (!curBoard.FollowRule)
+            return;
+
         for(int i = 0; i < curBoard.places.GetLength(0); i++)
         {
             for (int j = 0; j < curBoard.places.GetLength(1); j++)
             {
-                // 좌표에 기물이 있다면
-                if (curBoard.places[i, j].piece != null)
-                {
-                    // 아군 기물이라면 건너 뛴다.
-                    continue;
 
-                    // 적군 기물이라면 잡을 수 있다고 표시한다.
-                }
 
-                Vector2Int index = new Vector2Int(i, j);
+                Vector2Int index = curBoard.places[i, j].boardIndex;
                 // 이동할 수 있는 영역인지 계산
                 if (selectedPiece.IsMovable(index))
                 {
                     // 이동할 수 있는 영역이라면
-                    curBoard.places[i, j].ChangeColor(highlight);
-                    Debug.Log("이동 가능!: " + curBoard.places[i, j].gameObject.name);
+                    Piece account = curBoard.places[i, j].piece;
+                    // 좌표에 기물이 있다면
+                    if (account != null)
+                    {
+                        // 아군 기물이라면
+                        if (account.team.TeamId == selectedPiece.team.TeamId)
+                        {
+                            selectedPiece.AddDefence(account);
+                            continue;
+                        }
+                        // 적군 기물이라면
+                        else
+                        {
+                            selectedPiece.AddAttack(account);
+                            curBoard.places[i, j].ChangeColor(attackable);
+                        }
+                    }
+                    else
+                    {
+                        curBoard.places[i, j].ChangeColor(highlight);
+                        //Debug.Log("이동 가능!: " + curBoard.places[i, j].gameObject.name);
+                        curBoard.places[i, j].IsApprochable = true;
+                    }
                 }
                 else
                 {
-                    Debug.Log("이동 불가능!: " + curBoard.places[i, j].gameObject.name);
+                    //Debug.Log("이동 불가능!: " + curBoard.places[i, j].gameObject.name);
                     // 이동할 수 있는 영역이 아니라면
+                    curBoard.places[i, j].IsApprochable = false;
                 }
 
             }
@@ -71,6 +104,7 @@ public class PlaceManager : SingleTon<PlaceManager>
             for (int j = 0; j < oldBoard.places.GetLength(1); j++)
             {
                 oldBoard.places[i, j].ChangeColor();
+                oldBoard.places[i, j].IsApprochable = true;
             }
 
         }
@@ -83,15 +117,23 @@ public class PlaceManager : SingleTon<PlaceManager>
 
         selectedPiece.SetInPlace(place);
         place.piece = selectedPiece;
-        
 
-        ControlInit();
-        ShowPlaceableEnd(oldPlace);
+
+        SelectedPieceInit();
     }
 
-    private void ControlInit()
+    public void SelectPiece(Piece piece)
     {
-        selectedPiece = null;
-       
+        SelectedPiece = piece;
+        ShowPlaceable();
+        GameManager.Instance.state = GameManager.GameState.SELECTING_PLACE;
+    }
+    public void SelectedPieceInit()
+    {
+        ShowPlaceableEnd(selectedPiece.place);
+        SelectedPiece = null;
+        GameManager.Instance.state = GameManager.GameState.SELECTING_PIECE;
+        // 소리도 나야 한다면
+        // OnExitSelect?.Invoke();
     }
 }
