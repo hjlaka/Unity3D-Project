4fc17e84a14130f5e0b0fd2a49e9f9f5448f7bec
HeejinLee <windamongtree@gmail.com>
2023-01-03 15:51:55 +0900

    test: 보드 관리용 HUD 추가

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Board : MonoBehaviour
@@ -13,5 +14,53 @@ public class Board : MonoBehaviour
 
     private bool followRule;
     public bool FollowRule { get; set; }
+    public Transform heatPointHUD;
+    private List<TextMeshProUGUI> heatHUDList;
     public Place[,] places;
+
+    private void Awake()
+    {
+        heatHUDList = new List<TextMeshProUGUI>();
+        
+    }
+    private void Start()
+    {
+
+        if (null == heatPointHUD)
+            return;
+
+        CreateHUD();
+        UpdateHeatHUD();
+    }
+
+    private void CreateHUD()
+    {
+        heatPointHUD.GetComponent<RectTransform>().sizeDelta = new Vector2(40 * size.x, 40 * size.y);
+
+        for (int i = 0; i < size.x * size.y; i++)
+        {
+            GameObject textUI = Instantiate(new GameObject(), heatPointHUD);
+            textUI.AddComponent<TextMeshProUGUI>();
+            TextMeshProUGUI text = textUI.GetComponent<TextMeshProUGUI>();
+            text.text = i.ToString();
+            text.fontSize = 30f;
+            //TextMeshProUGUI text = heatPointHUD.GetChild(i).GetComponent<TextMeshProUGUI>();
+            heatHUDList.Add(text);
+        }
+
+        heatPointHUD.gameObject.SetActive(false);
+    }
+    public void UpdateHeatHUD()
+    {
+        if (null == heatPointHUD)
+            return;
+
+        for (int i = 0; i < size.x; i++)
+        {
+            for(int j = 0; j < size.y; j++)
+            {
+                heatHUDList[(i * size.x) + j].text = places[i, j].HeatPoint.ToString();
+            }
+        }
+    }
 }
