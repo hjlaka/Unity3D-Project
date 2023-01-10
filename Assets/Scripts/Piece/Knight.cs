a100401589c70678826803bd92513ba4a6dfbb07
HeejinLee <windamongtree@gmail.com>
2023-01-10 17:19:50 +0900

    feat: 나이트 클래스 추가

--- Assets/Scripts/Piece/Knight.cs
+++ Assets/Scripts/Piece/Knight.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knight : MonoBehaviour
+public class Knight : Piece
 {
-    // Start is called before the first frame update
-    void Start()
+    public override void IsMovable(Vector2Int location)
     {
-        
+
+        Vector2Int boardSize = place.board.Size;
+
+        JumpPosition(location + new Vector2Int(2, 1), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(2, -1), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(-2, 1), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(-2, -1), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(1, 2), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(-1, 2), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(1, -2), boardSize.y, boardSize.x);
+        JumpPosition(location + new Vector2Int(-1, -2), boardSize.y, boardSize.x);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void JumpPosition(Vector2Int curLocation, int boardHeight, int boardWidth)
     {
-        
+        if (curLocation.x < 0 || curLocation.x > boardWidth - 1) return;
+        if (curLocation.y < 0 || curLocation.y > boardHeight - 1) return;
+
+        if (RecognizePiece(curLocation)) return;
     }
+
 }
