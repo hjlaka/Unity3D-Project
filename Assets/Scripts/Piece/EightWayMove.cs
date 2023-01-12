ed19689150615e991ea7dec266f7344139634aba
HeejinLee <windamongtree@gmail.com>
2023-01-12 19:37:51 +0900

    feat: 퀸과 퀸 움직임 클래스 추가
    복합 움직임을 형성하는 다른 방법은?

---
+++ Assets/Scripts/Piece/EightWayMove.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EightWayMove : MonoBehaviour, IPieceMovable
+{
+    IPieceMovable straight;
+    IPieceMovable diagonal;
+    //private Piece controlled;
+
+    public EightWayMove(Piece controlled)
+    {
+        //this.controlled = controlled;
+        straight = new StraightMove(controlled);
+        diagonal = new DiagonalMove(controlled);
+
+    }
+    public void RecognizeRange(Vector2Int location)
+    {
+        straight.RecognizeRange(location);
+        diagonal.RecognizeRange(location);
+        // 현재까지는 장소가 중복되는 데에 대한 대처가 없다.
+    }
+
+    public void RecognizeSpecialMove(Place newPlace)
+    {
+        // Do Nothing
+    }
+}
--- Assets/Scripts/Piece/Queen.cs
+++ Assets/Scripts/Piece/Queen.cs
@@ -2,17 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Queen : MonoBehaviour
+public class Queen : Piece
 {
-    // Start is called before the first frame update
-    void Start()
+    protected override void Awake()
     {
-        
-    }
+        base.Awake();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        movePattern = new EightWayMove(this);
     }
 }
