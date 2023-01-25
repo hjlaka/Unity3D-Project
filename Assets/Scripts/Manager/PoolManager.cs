5260e892ee9a9c73eb549f9d055e8b77bdc871e2
HeejinLee <windamongtree@gmail.com>
2023-01-25 21:51:35 +0900

    test: 테스트

--- Assets/Scripts/Manager/PoolManager.cs
+++ Assets/Scripts/Manager/PoolManager.cs
@@ -2,27 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour
+public class PoolManager : SingleTon<PoolManager>
 {
+    public struct PoolableInfo
+    {
+        public GameObject prefab;
+        public int count;
+    }
 
-    private Dictionary<string, GameObject> pools;
+    [SerializeField]
+    private List<PoolableInfo> poolableList = new List<PoolableInfo>();
 
+    private Dictionary<string, Stack<GameObject>> poolDic;
+
+    private void Awake()
+    {
+        poolDic = new Dictionary<string, Stack<GameObject>();
+    }
     private void Start()
     {
         
     }
     private void CreatePool()
     {
+        for(int i = 0; i < poolableList.Count; i++)
+        {
+            Stack<GameObject> stack = new Stack<GameObject>();
 
+            for(int j = 0; j < poolableList[i].count; j++)
+            {
+                GameObject instance = Instantiate(poolableList[i].prefab);
+                instance.SetActive(false);
+                stack.Push(instance);
+            }
+            poolDic.Add(poolableList[i].prefab.name, stack);
+  
+        }
     }
-    public void Get()
+    public GameObject Get(GameObject prefab)
     {
+        Stack<GameObject> stack = poolDic[prefab.name];
+        if(stack?.Count > 0)
+        {
+            GameObject instance = stack.Pop();
+            instance.gameObject.SetActive(true);
+            return instance;
+        }
 
+        return null;
     }
 
-    public void Return()
+    public void Return(GameObject instance)
     {
-
+        Stack<GameObject> stack = poolDic[instance.name];
+        instance.SetActive(false);
+        stack.Push(instance);
     }
 
 }
