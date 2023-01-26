b4cc36a5f9349752979d54bb11a68bfce7107396
HeejinLee <windamongtree@gmail.com>
2023-01-27 04:49:22 +0900

    style: 중복 함수 통합

--- Assets/Scripts/DecidePlaceStrategy/AttackFirstStrategy.cs
+++ Assets/Scripts/DecidePlaceStrategy/AttackFirstStrategy.cs
@@ -10,42 +10,8 @@ public class AttackFirstStrategy : DecidePlaceStrategy
     }
     public override Placement DecidePlace(Piece piece, ref float will, out ScoreNode scoreSet)
     {
-        float maxScore = -99f;
-        Place highScorePlace = null;
-        ScoreNode highScoreSet = null;
-
-        List<Place> movablePlaces = piece.Recognized.movable;
-
-        CopyData();
-     
-
-        for (int i = 0; i < movablePlaces.Count; i++)
-        {
-            Place place = movablePlaces[i];
-            ScoreNode tempScoreSet = CalculateScore(piece, place);
-            float score = tempScoreSet.WillPoint;
-
-            // 저장해 둔 점수 중 가장 높은 점수 가져오기? (왕 보호에 대한 것 외에는 차선을 가져올 수도 있도록?)
-            // 점수 저장이 의미 있을까?
-            if(score >= maxScore)
-            {
-                highScorePlace = place;
-                maxScore = score;
-                highScoreSet = tempScoreSet;
-            }
-        }
-        if(GameManager.Instance.scoreDebugMode)
-        {
-            Debug.Log(highScorePlace?.boardIndex + "가 최대 점수다. : " + maxScore);
-        }
-
-
-        will = maxScore / GetTotalWeight();
-        scoreSet = highScoreSet;
-
-        Placement placement = new Placement(piece, piece.place, highScorePlace, highScorePlace?.Piece, null);
-
-        return placement;
+       CopyData();
+       return base.DecidePlace(piece, ref will, out scoreSet);
     }
    /* public Place DecidePlace2(Piece piece)
     {
--- Assets/Scripts/DecidePlaceStrategy/DecidePlaceStrategy.cs
+++ Assets/Scripts/DecidePlaceStrategy/DecidePlaceStrategy.cs
@@ -21,9 +21,10 @@ public abstract class DecidePlaceStrategy : IDecidePlaceStrategy
 
     protected float totalWeight;
 
-    public abstract Placement DecidePlace(Piece piece, ref float will, out ScoreNode scoreSet);
     protected virtual void CopyData()
     {
+        if(strategyData == null) return;
+
         willingToDefend = strategyData.willingToDefend;
         willingToAttack = strategyData.willingToAttack;
         willingToThreat = strategyData.willingToThreat;
@@ -39,7 +40,52 @@ public abstract class DecidePlaceStrategy : IDecidePlaceStrategy
 
         return totalWeight;
     }
+    public virtual Placement DecidePlace(Piece piece, ref float will, out ScoreNode scoreSet)
+    {
+        float maxScore = -99f;
+        Place highScorePlace = null;
+        ScoreNode highScoreSet = null;
 
+        List<Place> movablePlaces = piece.Recognized.movable;
+
+        Debug.Log("갈 수 있는 자리 수: " + movablePlaces.Count);
+
+        if (movablePlaces.Count <= 0)
+        {
+            scoreSet = null;
+            return null;
+        }
+            
+        for (int i = 0; i < movablePlaces.Count; i++)
+        {
+            Place place = movablePlaces[i];
+            ScoreNode tempScoreSet = CalculateScore(piece, place);
+            Debug.Log("의지 점수: " + tempScoreSet.WillPoint);
+            float score = tempScoreSet.WillPoint;
+
+            // 저장해 둔 점수 중 가장 높은 점수 가져오기? (왕 보호에 대한 것 외에는 차선을 가져올 수도 있도록?)
+            // 점수 저장이 의미 있을까?
+            if (score >= maxScore)
+            {
+                highScorePlace = place;
+                maxScore = score;
+                highScoreSet = tempScoreSet;
+            }
+        }
+        if (GameManager.Instance.scoreDebugMode)
+        {
+            Debug.Log(highScorePlace?.boardIndex + "가 최대 점수다. : " + maxScore);
+        }
+
+
+        will = maxScore / GetTotalWeight();
+        scoreSet = highScoreSet;
+
+        Debug.Log(string.Format("가장 의지가 높은 자리 {0} 의지 점수 {1}", highScorePlace, will));
+        Placement placement = new Placement(piece, piece.place, highScorePlace, highScorePlace?.Piece, null);
+
+        return placement;
+    }
 
     protected virtual ScoreNode CalculateScore(Piece piece, Place place)
     {
