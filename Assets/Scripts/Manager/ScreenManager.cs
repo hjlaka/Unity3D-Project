a46c5bc58c8accf28c859541345d815e8d9a0923
HeejinLee <windamongtree@gmail.com>
2023-03-16 21:19:57 +0900

    chore: 기본 해상도 변경

--- Assets/Scripts/Manager/ScreenManager.cs
+++ Assets/Scripts/Manager/ScreenManager.cs
@@ -7,7 +7,8 @@ public class ScreenManager : SingleTon<ScreenManager>
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(600, 800, true);
+        Screen.SetResolution(1920, 1080, true);
+        //Screen.SetResolution(540, 960, true);
     }
 
 }
